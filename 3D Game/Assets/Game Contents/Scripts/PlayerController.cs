65e73d3c39ba88ae9be3924f397deba1735c7ead
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-11 13:17:45 +0100

    Commit007 - Motion of the car improved
    Forces applied never decelerate so means handling is hard

--- 3D Game/Assets/Game Contents/Scripts/PlayerController.cs
+++ 3D Game/Assets/Game Contents/Scripts/PlayerController.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables
-    private float speed = 15.0f;
-    private float turnSpeed = 35.0f;
+    // private float speed = 15.0f; No longer needed as using forces
+    private float turnSpeed = 70.0f;
     private float horizontalInput;
     private float forwardInput;
 
     // Making the car use forces to look realistic
     [SerializeField] private float horsePower = 10000;
     private Rigidbody playerRB;
+    [SerializeField] GameObject centreOfMass;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+
+        // Set the centre of mass of the rigid body to be where we put one on the vehicle
+        playerRB.centerOfMass = centreOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -31,7 +36,6 @@ public class PlayerController : MonoBehaviour
         // Move the vehicle
         //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-
         playerRB.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
     }
 }
