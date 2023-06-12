bf2b7ac0379e048b94b02e027eef06c76440ff12
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-12 16:25:34 +0100

    Commit047 - Pause menu functionality implemented

--- 3D Game/Assets/Game Contents/Scripts/PauseMenu.cs
+++ 3D Game/Assets/Game Contents/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -44,12 +45,13 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting game...");
+        Application.Quit();
     }
 
 }
