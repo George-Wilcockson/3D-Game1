93b149465bdd380c405cbd32c7b48dde111d9830
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-12 09:27:10 +0100

    Commit015 - Camera follows
    Follows but lags too far behind

--- 3D Game/Assets/Game Contents/Scripts/CameraFollow.cs
+++ 3D Game/Assets/Game Contents/Scripts/CameraFollow.cs
@@ -1,21 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 4, -10);
+    // The player to follow
+    public GameObject target = null;
+    // The target view
+    public GameObject t = null;
 
-    // Start is called before the first frame update
-    void Start()
+    // Speed of camera
+    public float speed = 1.5f;
+
+    // For camera switching later on
+    public int index;
+
+
+    private void Start()
     {
+        // Sets the right objects to be used
+        target = GameObject.FindGameObjectWithTag("Player");
+        t = GameObject.FindGameObjectWithTag("Target");
 
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void FixedUpdate()
     {
-        transform.position = player.transform.position + offset;
+        this.transform.LookAt(target.transform);
+        float car_move = Mathf.Abs(Vector3.Distance(this.transform.position, t.transform.position) * speed);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, t.transform.position, car_move * Time.deltaTime);
     }
 }
