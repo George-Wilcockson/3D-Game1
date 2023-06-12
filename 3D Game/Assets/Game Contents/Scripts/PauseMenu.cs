88f28b3599c0d7af2348f47299f11c0f1c99aa08
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-12 16:15:14 +0100

    Commit045 - Initial pause menu implemented
    Esc pauses and resumes game

---
+++ 3D Game/Assets/Game Contents/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Public so can see from elsewhere, static because not about referencing this script, just checking
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // If its already paused then resume
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f; // Continues in background in slow mo
+
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f; // Continues in background in slow mo
+
+        gamePaused = true;
+    }
+
+}
