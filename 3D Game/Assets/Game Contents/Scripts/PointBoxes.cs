16a2a666f8a0ce7866626cbec425456fcbec2ac8
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-13 11:23:23 +0100

    Commit049 - Score system added, basic not linked to boxes
    No objects to score from yet, but structure there to add "detect collision" and it will work

---
+++ 3D Game/Assets/Game Contents/Scripts/PointBoxes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointBoxes : MonoBehaviour
+{
+    // reference to our scoring class
+    public Scoring score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For now, no boxes so we just add a score when P is pushed
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            score.AddScore(1);
+        }
+    }
+}
